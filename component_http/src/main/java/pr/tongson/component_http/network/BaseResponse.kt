8218b0c2dcf77e45f8d14a7ed0e1bcc6306de484
Tongson <tongsonloo@gmail.com>
2020-04-26 23:28:47 +0800

    BaseHttp

---
+++ component_http/src/main/java/pr/tongson/component_http/network/BaseResponse.kt
@@ -0,0 +1,12 @@
+package pr.tongson.component_http.network
+
+/**
+ * @Email:289286298@qq.com
+ * @Author tongson
+ * @Date 2020/4/26
+ * @Version
+ * @Since
+ * @Description
+ */
+class BaseResponse {
+}
---
+++ component_http/src/main/java/pr/tongson/component_http/utils/NetworkUtils.java
@@ -0,0 +1,12 @@
+package pr.tongson.component_http.utils;
+
+/**
+ * @Email:289286298@qq.com
+ * @Author tongson
+ * @Date 2020/4/26
+ * @Version
+ * @Since
+ * @Description
+ */
+public class NetworkUtils {
+}
